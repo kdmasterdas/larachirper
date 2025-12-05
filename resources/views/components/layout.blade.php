65fc6d11d84b95502fad3cbc8c62a9fe21de36d7
Kalyan Das <kalyan@capitalnumbers.com>
2025-12-05 15:50:21 +0530

    css change

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -82,7 +82,7 @@
         </div>
     @endif
 
-    <main class="flex-1 container mx-auto px-4 py-8 !mt-8">
+    <main class="flex-1 container mx-auto px-4 py-8 mt-8!">
         {{ $slot }}
     </main>
 
