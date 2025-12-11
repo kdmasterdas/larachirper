7c00b7459e37978bdca786da35725a1d871b4c0a
Kalyan Das <kalyan@capitalnumbers.com>
2025-12-11 14:15:20 +0530

    PRS-12

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,37 +6,36 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-Use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
-
     use AuthorizesRequests;
     /**
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
     {
-        
+
         $validate = $request->validate(
             [
                 'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
                 'password' => 'required|string|confirmed|min:8|confirmed'
             ]
-            );
+        );
 
-            $user = User::create(
-                [
-                    'name' => $validate['name'],
-                    'email' => $validate['email'],
-                    'password' => Hash::make($validate['password'])
-                ]
-            );
+        $user = User::create(
+            [
+                'name' => $validate['name'],
+                'email' => $validate['email'],
+                'password' => Hash::make($validate['password'])
+            ]
+        );
 
-            Auth::login($user);
+        Auth::login($user);
 
-            return redirect('/')->with('success', 'Registration successfull!');
+        return redirect('/')->with('success', 'Registration successfull!');
     }
 }
